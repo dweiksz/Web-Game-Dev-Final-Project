cc0151d2dadb42a7ecf36beef35f09f07b4c6fe2
Saleem Dweik <54520939+saleemdweik@users.noreply.github.com>
2021-11-16 12:48:33 -0500

    Redid scripts to fix jumping issue

--- scripts.js
+++ scripts.js
@@ -1,23 +1,28 @@
-var player = document.getElementById("player");
-var obstacle = document.getElementById("obstacle");
-var counter=0;
-function jump(){
-    if(player.classList == "animate"){return}
-    player.classList.add("animate");
-    setTimeout(function(){
-        player.classList.remove("animate");
-    },300);
+const player = document.getElementById("player");
+const obstacle = document.getElementById("obstacle");
+
+function jump() {
+  if (player.classList != "jump") {
+    player.classList.add("jump");
+
+    setTimeout(function () {
+        player.classList.remove("jump");
+    }, 300);
+  }
 }
-var checkDead = setInterval(function() {
-    let characterTop = parseInt(window.getComputedStyle(player).getPropertyValue("top"));
-    let blockLeft = parseInt(window.getComputedStyle(obstacle).getPropertyValue("left"));
-    if(blockLeft<20 && blockLeft>-20 && characterTop>=130){
-        block.style.animation = "none";
-        alert("Game Over. score: "+Math.floor(counter/100));
-        counter=0;
-        block.style.animation = "block 1s infinite linear";
-    }else{
-        counter++;
-        document.getElementById("scoreSpan").innerHTML = Math.floor(counter/100);
-    }
+
+let isAlive = setInterval(function () {
+  let playerTop = parseInt(window.getComputedStyle(player).getPropertyValue("top"));
+
+  let obstacleLeft = parseInt(
+    window.getComputedStyle(obstacle).getPropertyValue("left")
+  );
+
+  if (obstacleLeft < 50 && obstacleLeft > 0 && playerTop >= 140) {
+    alert("Game Over!");
+  }
 }, 10);
+
+document.addEventListener("keydown", function (event) {
+  jump();
+});
